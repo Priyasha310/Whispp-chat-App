829f283784d80aa62c6bf97b28a17f1447e6ce3e
Priyasha310 <72645022+Priyasha310@users.noreply.github.com>
2023-10-16 21:57:29 +0530

    register functionality done with validation

---
+++ backend/controllers/auth.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+const {StatusCodes} = require('http-status-codes');
+const {UnauthenticatedError} = require('../errors');
+
+const register = async (req, res, next) => {
+    console.log(req.body);
+    // const user = await User.create({...req.body})    
+    const {username, email,  password, confirmPassword} = req.body; 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+        confirmPassword: hashedPassword,
+      });
+    const token = user.createJWT();
+    res
+        .status(StatusCodes.CREATED)
+        .json({user:{ usernname:user.username, email: user.email }})
+
+}
+
+module.exports = {register}
