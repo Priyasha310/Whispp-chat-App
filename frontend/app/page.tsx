07a17abf4de5980bb65f30a1e9c9afde57d910fc
Priyasha310 <72645022+Priyasha310@users.noreply.github.com>
2023-10-17 13:11:07 +0530

    config of project

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Chat from '@/pages/chat'
+import Register from '@/pages/register'
+import React from 'react'
+
+const Home = () => {
+  return (
+    "Home Page"
+  )
+}
+
+export default Home
